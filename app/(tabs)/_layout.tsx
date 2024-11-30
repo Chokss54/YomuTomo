6d445262de97a843dcf98405b209d9595b9154f3
Chokss54 <joelyang54@gmail.com>
2024-12-01 01:26:18 +1100

    upload codebase & file structure

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,77 @@
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons';
+import { HapticTab } from "@/components/HapticTab";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: "absolute",
+          },
+          default: {},
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="home" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="notes"
+        options={{
+          title: "Notes",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="view-agenda" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="snap"
+        options={{
+          title: "Snap",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="camera" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "History",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="history" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="account-circle" size={28} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
